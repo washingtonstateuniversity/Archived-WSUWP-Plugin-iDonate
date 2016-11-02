0761b7cda6df48c4f9b5ac21c6a4f8a7196e0da6
Blair Lierman <blair.lierman@wsu.edu>
2016-11-02 11:09:11 -0700

    Add unit test for existence of the Fund post type.

--- tests/test-wsuwp-plugin-idonate.php
+++ tests/test-wsuwp-plugin-idonate.php
@@ -7,4 +7,12 @@ class WSUWP_Plugin_iDonate_Tests extends WP_UnitTestCase {
 	public function test_WSUWP_Plugin_iDonate_function() {
 		$this->assertTrue( true );
 	}
+
+	/**
+	 * Verify that the 'fund' Post Type created by the plugin exists
+	 */
+	public function test_WSUWP_Plugin_iDonate_Post_Type_Fund() {
+
+		$this->assertTrue(post_type_exists( 'fund' ));
+	}
 }
