c2a71ff9195844aba5e55d0ff91b5730e59e8242
Blair Lierman <blair.lierman@wsu.edu>
2016-11-10 16:04:26 -0800

    Fix several style violations

--- includes/class-wsuwp-shortcode-fundselector.php
+++ includes/class-wsuwp-shortcode-fundselector.php
@@ -98,12 +98,12 @@ class WSUWP_Plugin_iDonate_ShortCode_Fund_Selector {
 	*/
 	function wsuf_fundselector_enqueue_scripts() {
 
-		wp_enqueue_script( 'wsuf_fundselector_utils', plugins_url( '/wsuwp-shortcode-fundselector-utils.js', __FILE__ ), array('jquery'), '1.0', true );
+		wp_enqueue_script( 'wsuf_fundselector_utils', plugins_url( '/wsuwp-shortcode-fundselector-utils.js', __FILE__ ), array( 'jquery' ), '1.0', true );
 
-		wp_enqueue_script( 'wsuf_fundselector', plugins_url( '/wsuwp-shortcode-fundselector.js', __FILE__ ), array('jquery', 'jquery-ui-core', 'jquery-ui-autocomplete'), '1.0', true );
+		wp_enqueue_script( 'wsuf_fundselector', plugins_url( '/wsuwp-shortcode-fundselector.js', __FILE__ ), array( 'jquery', 'jquery-ui-core', 'jquery-ui-autocomplete' ), '1.0', true );
 
 		wp_localize_script( 'wsuf_fundselector', 'wpData', array(
-			'ajax_url' => admin_url( 'admin-ajax.php' )
+			'ajax_url' => admin_url( 'admin-ajax.php' ),
 		));
 	}
 
