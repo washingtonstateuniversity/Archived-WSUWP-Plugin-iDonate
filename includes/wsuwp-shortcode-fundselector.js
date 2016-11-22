ce805ee2684dd5562da8c51b1d4ddc9f474440d7
Blair Lierman <blair.lierman@wsu.edu>
2016-11-21 16:06:21 -0800

    Load funds when a subcategory is selected. Funds are loading via the REST API using taxonomy slug and category ID

--- includes/wsuwp-shortcode-fundselector.js
+++ includes/wsuwp-shortcode-fundselector.js
@@ -53,11 +53,45 @@ jQuery(document).ready(function($) {
 
 				var $list = $('#subcategories'); 
 				$list.html('<option disabled selected value> -- Select a Category -- </option>');
+				$('#funds').html('<option disabled selected value> -- Select a Fund -- </option>');
 				$.each(json, function(key, value) {   
 					$list
-					.append($('<option>', { value : value["id"] })
+					.append($('<option>', { value : value["id"], "data-category" : value["taxonomy"] })
 					.text( wsuwpUtils.htmlDecode(value["name"]) )); 
 				});
+
+			})
+		}
+
+		event.preventDefault();
+	});
+
+	
+	// Subcategory Click event
+	$("#subcategories")
+	.change( function( event ) {
+		var category = $(this).find(":selected").attr("data-category");
+		var subcategoryId = $(this).find(":selected").attr("value");
+
+		if(category && subcategoryId) {
+			// GET /wp-json/wp/v2/idonate_fund?<taxonomy_slug>=<category_id> (e.g. GET /wp-json/wp/v2/idonate_fund?idonate_priorities=35)
+			var restQueryUrl = wpData.request_url_base + 'idonate_fund?' + category + "=" + subcategoryId;
+			
+			$.getJSON( restQueryUrl )
+			.done(function( json ) {
+
+				var $list = $('#funds'); 
+				$list.html('<option disabled selected value> -- Select a Fund -- </option>');
+				$.each(json, function(key, value) {   
+					$list
+					.append($('<option>', { value : value["designationId"] })
+					.text( wsuwpUtils.htmlDecode(value["title"].rendered) ) ); 
+				});
+
+				if(json.length > 0)
+				{
+					$list.prop("disabled", false);
+				}
 
 			})
 		}
