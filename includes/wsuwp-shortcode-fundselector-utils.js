7daa97ece3c6c6ef0b3908789fc20ffe46afe393
Blair Lierman <blair.lierman@wsu.edu>
2016-11-22 16:31:00 -0800

    Don't allow duplicate funds in the Fund list. Check the designation ID to make sure each one in the list is unique.

--- includes/wsuwp-shortcode-fundselector-utils.js
+++ includes/wsuwp-shortcode-fundselector-utils.js
@@ -7,7 +7,26 @@ window.wsuwpUtils = window.wsuwpUtils || {};
 
 		addListItem: function ( $list, name, designationId ) {
 			var html = '<li class="list-group-item" data-designation_id="' + designationId + '">' + name + '<a href="#" class="pull-right"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span><span class="sr-only">Remove Fund button</span></a></li>'; 
-			$list.append(html);
+			
+			if(!this.isDuplicateDesignation(designationId, $list))
+			{
+				$list.append(html);
+			}
+		},
+
+		isDuplicateDesignation: function (designationId, $list)
+		{
+			var duplicate = false;
+			$list.each(function()
+			{
+				if(designationId == jQuery(this).find("li").attr("data-designation_id"))
+				{ 
+					duplicate = true;
+					return false;
+				}
+			});
+
+			 return duplicate;
 		},
 
 		enableButton: function ( $button ) {
