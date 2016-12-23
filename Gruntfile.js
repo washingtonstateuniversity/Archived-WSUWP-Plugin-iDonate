186bcb02e3715efead4a3157a1cd0f76035d9c12
Jeremy Felt <jeremy.felt@gmail.com>
2016-12-23 13:55:48 -0800

    Add configuration for local server via Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,11 +10,35 @@ module.exports = function(grunt) {
                 bin: "vendor/bin/phpcs --extensions=php --ignore=\"*/vendor/*,*/node_modules/*\"",
                 standard: "phpcs.ruleset.xml"
             }
+        },
+
+        watch: {
+            styles: {
+                files: [ "css/*.css", "src/js/*.js" ],
+                tasks: [ "default" ],
+                option: {
+                    livereload: 8000
+                }
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    open: true,
+                    port: 8000,
+                    hostname: "localhost"
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-phpcs');
+    grunt.loadNpmTasks( "grunt-contrib-watch" );
+    grunt.loadNpmTasks( "grunt-contrib-connect" );
 
     // Default task(s).
     grunt.registerTask('default', ['phpcs']);
+
+    grunt.registerTask( "serve", [ "connect", "watch" ] );
 };
