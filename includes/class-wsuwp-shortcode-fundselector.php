72702e93488b028612c4a666753be61bdfc2061c
Blair Lierman <blair.lierman@wsu.edu>
2017-01-04 15:09:55 -0800

    Move the Add Fund button under Dollar Amounts.

--- includes/class-wsuwp-shortcode-fundselector.php
+++ includes/class-wsuwp-shortcode-fundselector.php
@@ -105,13 +105,6 @@ class WSUWP_Plugin_iDonate_ShortCode_Fund_Selector {
 		</div>
 		';
 
-		// Add Fund Button
-		$return_string .= '
-			<input name="inpDesignationId" id="inpDesignationId" type="hidden">
-			<input name="inpFundName" id="inpFundName" type="hidden">
-			<button id="addFundButton" type="button">Add Fund</button>
-		';
-
 		// Dollar Amount Selectors
 		$return_string .= '
 		<div class="" role="group">
@@ -127,6 +120,13 @@ class WSUWP_Plugin_iDonate_ShortCode_Fund_Selector {
 			</div>
 			<input name="inpAmount" id="inpAmount" class="value" data-token="amount" value="25" type="hidden">
 		</div>
+		';
+
+		// Add Fund Button
+		$return_string .= '
+			<input name="inpDesignationId" id="inpDesignationId" type="hidden">
+			<input name="inpFundName" id="inpFundName" type="hidden">
+			<button id="addFundButton" type="button">Add Fund</button>
 		';
 
 		// Selected Funds List
