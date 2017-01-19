d88c111ecded572b3fbbc2bbb2002ad8a0c06e05
Blair Lierman <blair.lierman@wsu.edu>
2017-01-19 15:06:08 -0800

    Fix style violations.

--- includes/class-wsuwp-shortcode-fundselector.php
+++ includes/class-wsuwp-shortcode-fundselector.php
@@ -81,11 +81,11 @@ class WSUWP_Plugin_iDonate_ShortCode_Fund_Selector {
 		$unit_priorities = $this->wsuf_fundselector_funds_get_funds( $args['unit_taxonomy'], $args['unit_category'] );
 		$unit_priorities_list = '<option disabled selected value> SELECT A FUND </option>';
 
-				foreach ( $unit_priorities as $unit_priority ) {
-				$fund_name = esc_html( $unit_priority['fund_name'] );
-				$fund_designation_id = esc_attr( $unit_priority['designation_id'] );
-				$unit_priorities_list .= "<option value=\"{$fund_designation_id}\">{$fund_name}</option>";
-			}
+		foreach ( $unit_priorities as $unit_priority ) {
+			$fund_name = esc_html( $unit_priority['fund_name'] );
+			$fund_designation_id = esc_attr( $unit_priority['designation_id'] );
+			$unit_priorities_list .= "<option value=\"{$fund_designation_id}\">{$fund_name}</option>";
+		}
 
 		$unit_description = ! empty( $args['unit_description'] ) ? $args['unit_description'] : 'Please choose a fund to support';
 
@@ -101,11 +101,11 @@ class WSUWP_Plugin_iDonate_ShortCode_Fund_Selector {
 		$priorities = $this->wsuf_fundselector_funds_get_funds( 'idonate_priorities', 'idonate_priorities' );
 		$priorities_list = '<option disabled selected value> SELECT A FUND </option>';
 
-				foreach ( $priorities as $priority ) {
-				$fund_name = esc_html( $priority['fund_name'] );
-				$fund_designation_id = esc_attr( $priority['designation_id'] );
-				$priorities_list .= "<option value=\"{$fund_designation_id}\">{$fund_name}</option>";
-			}
+		foreach ( $priorities as $priority ) {
+			$fund_name = esc_html( $priority['fund_name'] );
+			$fund_designation_id = esc_attr( $priority['designation_id'] );
+			$priorities_list .= "<option value=\"{$fund_designation_id}\">{$fund_name}</option>";
+		}
 
 		$return_string .= '
 		<div id="prioritiesTab" class="categoryTab wrapper ' . ($unit_included ? 'hidden' : '') . '">    
