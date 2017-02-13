0da99d90b0a3420a3f7507acd88250a05b51f8a3
Blair Lierman <blair.lierman@wsu.edu>
2017-02-13 13:06:37 -0800

    Add and remove scholarship fund based on checkbox. Add a new optional scholarship class to the list item for easier removal

--- includes/wsuwp-shortcode-fundselector.js
+++ includes/wsuwp-shortcode-fundselector.js
@@ -177,6 +177,29 @@ jQuery(document).ready(function($) {
 		showForm();
 	});
 
+
+	$("#genScholarship").change(function() {
+		// this will contain a reference to the checkbox
+		if (this.checked) {
+			// the checkbox is now checked
+			$("#inpAmount").val($(this).attr("data-amount"));
+			$("#inpDesignationId").text($(this).attr("data-designation_id"));
+			$("#inpFundName").text($(this).attr("data-fund_name"));
+			addFundAction(true);
+		} else {
+			// the checkbox is now no longer checked
+			$("#selectedFunds > li.fund-scholarship").remove();
+
+			// If the Fund list is empty, disable the Continue Button
+			if ($("#selectedFunds").find("li").length === 0) {
+				wsuwpUtils.disableButton($("#continueButton"));
+				hideContinueButton();
+			}
+		}
+	});
+
+
+
 	loadPriorities($("#priorities"), "idonate_priorities", "idonate_priorities");
 	loadPriorities($("#unit-priorities"), wpData.unit_taxonomy, wpData.unit_category);
 });
@@ -205,14 +228,14 @@ function loadPriorities($list, category, subcategory)
 	}
 }
 
-function addFundAction()
+function addFundAction(scholarship)
 {
 	var designationId = jQuery("#inpDesignationId").text();
 	var fundName = jQuery("#inpFundName").text();
 
 	if(designationId && fundName)
 	{
-		wsuwpUtils.addListItem(jQuery("#selectedFunds"), fundName, designationId, jQuery("#inpAmount").val());
+		wsuwpUtils.addListItem(jQuery("#selectedFunds"), fundName, designationId, jQuery("#inpAmount").val(), scholarship);
 		wsuwpUtils.enableButton(jQuery("#continueButton"));
 		showContinueButton();
 		resetForm();
