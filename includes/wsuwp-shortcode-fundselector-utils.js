d933ccde25c28f887c0fa12db8b1a5f31a090b52
Blair Lierman <blair.lierman@wsu.edu>
2017-03-13 15:03:31 -0700

    Move the Edit Fund Amount error under fund info. Add a new span for the fund info and stack the error underneath it.

--- includes/wsuwp-shortcode-fundselector-utils.js
+++ includes/wsuwp-shortcode-fundselector-utils.js
@@ -7,11 +7,14 @@ window.wsuwpUtils = window.wsuwpUtils || {};
 
 		addListItem: function ( $list, name, designationId, amount, scholarship  ) {
 			var html = '<li class="list-group-item ' + (scholarship ? "fund-scholarship": "") + '" data-designation_id="' + designationId + '" data-amount="' + amount + '">';
-			html += '<span class="right">' + _.escape(name) + '</span>'
-			html += '<span class="center">$</span><span id="edit' + designationId + '" class="editable left">' + amount +  '</span>';
-			html += '<span class="edit"><a href="#!" id="' + designationId +'" class="edit">EDIT</a></span>';
-			html += '<span class="close remove"><a href="#"></a></span>'
-			html += '<span id="error' + designationId + '" class="error"></span></li>';
+			html += '<span class="fund-info">';
+			html += '	<span class="right">' + _.escape(name) + '</span>'
+			html += '	<span class="center">$</span><span id="edit' + designationId + '" class="editable left">' + amount +  '</span>';
+			html += '	<span class="edit"><a href="#!" id="' + designationId +'" class="edit">EDIT</a></span>';
+			html += '	<span class="close remove"><a href="#"></a></span>'
+			html += '</span>';
+			html += '<span id="error' + designationId + '" class="error"></span>';
+			html += '</li>';
 			
 			if(!wsuwpUtils.isDuplicateDesignation(designationId, $list))
 			{
@@ -31,7 +34,7 @@ window.wsuwpUtils = window.wsuwpUtils || {};
 						jQuery("#error" + designationId).text("Amount must be between $3 and $100,000. Amount was reset.");
 					}
 					else{
-						e.target.parent().attr("data-amount", e.value);
+						e.target.parent().parent().attr("data-amount", e.value);
 						jQuery("#error" + designationId).text("");
 
 						wsuwpUtils.updateTotalAmountText();
