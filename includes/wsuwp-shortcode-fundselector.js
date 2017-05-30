b3e6126b61b44ce37739483345525ad75f397976
wsuf.student.01@wsu.edu <wsuf.student.01@wsu.edu>
2017-05-30 14:51:01 -0700

    Add a loading modal to the subcategories and fund list, as well as the search

--- includes/wsuwp-shortcode-fundselector.js
+++ includes/wsuwp-shortcode-fundselector.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
 	$("#fundSearch").autocomplete(
         {
 			source: function( request, response ) {
-	
+				$("#fundSearch").addClass("loading");
 				$.getJSON( wpData.request_url_base + 'idonate_fund', 
 					{
 						search : request.term
@@ -20,7 +20,7 @@ jQuery(document).ready(function($) {
 								"value": wsuwpUtils.htmlDecode(fund.title.rendered)
 							};
 						});
-						
+						$("#fundSearch").removeClass("loading");
 						response( fundList );
 					}
 				);
@@ -49,25 +49,30 @@ jQuery(document).ready(function($) {
 
 		if(categoryName) {
 			var restUrl = wpData.request_url_base + encodeURIComponent(categoryName);
-			
+
+			var $list = $('#subcategories');
+			$list.prop('disabled', true); 
+			$list.addClass('loading');
+			$list.removeClass('fund');
+
 			$.getJSON( restUrl )
 			.done(function( json ) {
 
-				var $list = $('#subcategories'); 
 				$list.empty();
 				$list.append('<option disabled selected value> SELECT A CATEGORY </option>');
 				
 				var $fundList = $('#funds');				
 				$fundList.empty();
 				$fundList.append('<option disabled selected value> SELECT A FUND </option>');
-				$fundList.prop("disabled", true);
 
 				$.each(json, function(key, value) {   
 					$list
 					.append($('<option>', { value : value["id"], "data-category" : value["taxonomy"] })
 					.text( wsuwpUtils.htmlDecode( value["name"]) )); 
 				});
-
+				$list.prop('disabled', false);
+				$list.addClass('fund');
+				$list.removeClass('loading'); 
 			})
 		}
 
@@ -85,10 +90,14 @@ jQuery(document).ready(function($) {
 			// GET /wp-json/wp/v2/idonate_fund?<taxonomy_slug>=<category_id> (e.g. GET /wp-json/wp/v2/idonate_fund?idonate_priorities=35)
 			var restQueryUrl = wpData.request_url_base + 'idonate_fund?' + category + "=" + subcategoryId;
 			
+			var $list = $('#funds'); 
+			$list.prop('disabled', true); 
+			$list.addClass('loading');
+			$list.removeClass('fund');
+
 			$.getJSON( restQueryUrl )
 			.done(function( json ) {
 
-				var $list = $('#funds'); 
 				$list.empty();
 				$list.append('<option disabled selected value> SELECT A FUND </option>');
 				$.each(json, function(key, value) {   
@@ -100,6 +109,8 @@ jQuery(document).ready(function($) {
 				if(json.length > 0)
 				{
 					$list.prop("disabled", false);
+					$list.addClass('fund');
+					$list.removeClass('loading'); 
 				}
 
 			})
