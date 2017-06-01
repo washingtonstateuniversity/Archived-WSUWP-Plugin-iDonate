332c021413f2536083bf04f7e757295b9b90b0dd
wsuf.student.01@wsu.edu <wsuf.student.01@wsu.edu>
2017-06-01 09:17:24 -0700

    Round gift amounts to two decimal places, and compare the entered amount to actual values, rather than the length of the number the user entered

--- includes/wsuwp-shortcode-fundselector.js
+++ includes/wsuwp-shortcode-fundselector.js
@@ -129,10 +129,10 @@ jQuery(document).ready(function($) {
 	$("#addFundButton")
 	.click( function ( event ) {	
 		var amount = jQuery("#otherAmount").val();
-
-		if(wsuwpUtils.validateAmount(amount))
+		var roundedAmount = wsuwpUtils.roundAmount(amount);
+		if(wsuwpUtils.validateAmount(roundedAmount, minimumGift, maximumGift))
 		{	
-			jQuery("#inpAmount").val( amount );
+			jQuery("#inpAmount").val( roundedAmount );
 			addFundAction();
 		}
 	});
@@ -185,16 +185,17 @@ jQuery(document).ready(function($) {
 	// Other Amount text field Change Event
 	$("#otherAmount").on('input propertychange paste', function () {
 		var inputAmount = $(this).val();
-		if(wsuwpUtils.validateAmount(inputAmount))
+		var roundedAmount = wsuwpUtils.roundAmount(inputAmount);
+		if(wsuwpUtils.validateAmount(roundedAmount, minimumGift, maximumGift))
 		{
-			$("#inpAmount").val(inputAmount);
+			$("#inpAmount").val(roundedAmount);
 			wsuwpUtils.enableButton($("#addFundButton"));
 			jQuery("#errorOtherAmount").text("");
 		}
 		else
 		{
 			wsuwpUtils.disableButton($("#addFundButton"));
-			jQuery("#errorOtherAmount").text("Amount should be between $3 and $100,000.");
+			jQuery("#errorOtherAmount").text("Amount should be between $" + minimumGift + " and $" + maximumGift);
 		}		
 	});
 
