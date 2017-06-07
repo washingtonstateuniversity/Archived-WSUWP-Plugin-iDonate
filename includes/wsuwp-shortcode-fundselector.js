6a4997c76f7f7e65eb3bc892f09a791511dc84c0
wsuf.student.01@wsu.edu <wsuf.student.01@wsu.edu>
2017-06-07 11:39:41 -0700

    add focus to the total gift text, the other fund amount input, and the add fund button keypress event

--- includes/wsuwp-shortcode-fundselector.js
+++ includes/wsuwp-shortcode-fundselector.js
@@ -48,6 +48,7 @@ jQuery(document).ready(function($) {
 		var categoryName = $(this).attr("data-category");
 
 		if(categoryName) {
+			$('#subcategories').focus();
 			var restUrl = wpData.request_url_base + encodeURIComponent(categoryName);
 			
 			$.getJSON( restUrl )
@@ -123,6 +124,7 @@ jQuery(document).ready(function($) {
 	.click( function ( event ) {
 		handleAmountSelectionClick( event );	
 		addFundAction();
+		$('#selectedFunds').focus();
 	});
 
 	// Add Fund Button click  event
@@ -134,6 +136,14 @@ jQuery(document).ready(function($) {
 		{	
 			jQuery("#inpAmount").val( amount );
 			addFundAction();
+		}
+	});
+
+	// Add Fund Button keypress event
+	$("#addFundButton")
+	.keypress(function (e) {
+		if (e.which == 13) { // enter key is pressed
+			$('#addFundButton').click();
 		}
 	});
 
@@ -202,6 +212,8 @@ jQuery(document).ready(function($) {
 		$(".amount-selection.selected").removeClass("selected");
 		$(this).addClass("selected");
 		showOther();
+		$('#otherAmount').focus();
+		$('#addFundButton').attr('tabindex', "0")
 	});
 
 	// Continue Button Initialization and Click Event
