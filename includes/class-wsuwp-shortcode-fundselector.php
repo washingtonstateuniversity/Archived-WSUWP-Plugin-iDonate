daa28b040b9946b066e71339619f7eec50c8dd40
Blair Lierman <blair.lierman@wsu.edu>
2018-02-19 14:49:08 -0800

    Only get published posts for list of funds Have the server check permissions to only get published posts.

--- includes/class-wsuwp-shortcode-fundselector.php
+++ includes/class-wsuwp-shortcode-fundselector.php
@@ -335,7 +335,8 @@ class WSUWP_Plugin_iDonate_ShortCode_Fund_Selector {
 	function wsuf_fundselector_funds_get_funds( $category, $subcategory ) {
 		$fund_list = get_posts(array(
 			'post_type'   => 'idonate_fund',
-			'post_status' => 'any',
+			'post_status' => 'publish',
+			'perm' => 'readable',
 			'posts_per_page' => -1, // Get all posts
 			'tax_query' => array(
 					array(
