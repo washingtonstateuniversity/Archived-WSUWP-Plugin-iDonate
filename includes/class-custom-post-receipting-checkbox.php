87aa1a89d89fd383cfa0522b8b6a1ff4e1cc6758
Jared Crain <jared.crain@wsu.edu>
2019-10-31 10:39:52 -0700

    Attempt to fix php lint issues

--- includes/class-custom-post-receipting-checkbox.php
+++ includes/class-custom-post-receipting-checkbox.php
@@ -20,49 +20,47 @@ class WSUWP_Plugin_iDonate_Post_Receipting_Checkbox {
 	 *
 	 * @since    1.2.3
 	 */
-    public function init() {
-		add_action('post_submitbox_misc_actions', array( $this, 'createCustomField' ) );
-		add_action('save_post', array( $this, 'saveCustomField' ) );
+	public function init() {
+		add_action( 'post_submitbox_misc_actions', array( $this, 'createCustomField' ) );
+		add_action( 'save_post', array( $this, 'saveCustomField' ) );
 	}
 
-	function createCustomField()
-	{
+	function createCustomField() {
 		$post_id = get_the_ID();
 
-		if (get_post_type($post_id) != 'idonate_fund') {
+		if ( get_post_type( $post_id ) !== 'idonate_fund' ) {
 			return;
 		}
 	
-		$value = get_post_meta($post_id, 'hideReceipt', true);
-		wp_nonce_field('hide_receipt_'.$post_id, 'hide_receipt');
+		$value = get_post_meta( $post_id, 'hideReceipt', true );
+		wp_nonce_field( 'hide_receipt_'.$post_id, 'hide_receipt' );
 
 		?>
 		<div class="misc-pub-section misc-pub-section-last">
-			<label><input type="checkbox" value="1" <?php checked($value, true, true); ?> name="hideReceipt" /><?php _e('Hide Email Receipts', 'pmg'); ?></label>
+			<label><input type="checkbox" value="1" <?php checked( $value, true, true ); ?> name="hideReceipt" /><?php esc_attr_e( 'Hide Email Receipts', 'pmg' ); ?></label>
 		</div>
 		<?php
 	}
 
-	function saveCustomField($post_id)
-	{
-		if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+	function saveCustomField( $post_id ) {
+		if (defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE) {
 			return;
 		}
 		
 		if (
-			!isset($_POST['hide_receipt']) ||
-			!wp_verify_nonce($_POST['hide_receipt'], 'hide_receipt_'.$post_id)
+			!isset( $_POST['hide_receipt'] ) ||
+			!wp_verify_nonce( $_POST['hide_receipt'], 'hide_receipt_' . $post_id )
 		) {
 			return;
 		}
 		
-		if (!current_user_can('edit_post', $post_id)) {
+		if (!current_user_can( 'edit_post', $post_id )) {
 			return;
 		}
-		if (isset($_POST['hideReceipt'])) {
-			update_post_meta($post_id, 'hideReceipt', $_POST['hideReceipt']);
+		if (isset( $_POST['hideReceipt'] )) {
+			update_post_meta( $post_id, 'hideReceipt', $_POST['hideReceipt'] );
 		} else {
-			delete_post_meta($post_id, 'hideReceipt');
+			delete_post_meta( $post_id, 'hideReceipt' );
 		}
 	}
 }
