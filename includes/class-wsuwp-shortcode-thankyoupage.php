8d9a01b1172de4455ce0f76728364acb3e2ef775
Blair Lierman <blair.lierman@wsu.edu>
2021-07-07 16:29:11 -0700

    Parse JSON for Donor query param in Thank You Page

--- includes/class-wsuwp-shortcode-thankyoupage.php
+++ includes/class-wsuwp-shortcode-thankyoupage.php
@@ -117,7 +117,14 @@ class WSUWP_Plugin_iDonate_ShortCode_ThankYouPage {
 		$return_array = array();
 		if ( ! empty( $_GET ) ) {
 
-			$query_params = $this->array_flatten( $_GET );
+			$query_params =  $_GET ;
+			// Parse Donor query param as JSON
+			$query_params["donor"] = json_decode(stripslashes(nl2br($query_params["donor"])), true);
+			
+			$query_params = $this->array_flatten( $query_params );
+			// $query_params["donor_url"] = urldecode($query_params["donor"]);
+			// $query_params["donor_strip"] = stripslashes(nl2br($query_params["donor"]));
+			// $query_params["donor_json"] = json_decode(stripslashes(nl2br($query_params["donor"])), true);
 
 			//loop over each category
 			foreach ( $query_params as $key => $qp ) {
